9bc7cca06fed94b08a7ebf7d75e7706cf82cb93a
Oliver <olichase91@gmail.com>
2023-04-25 15:19:57 -0700

    Yep

--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -7,15 +7,15 @@ class Test_Course:
 
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
+        course = Course("Programming Robots for Outer Space", "Full-Time", "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
         assert(course.title == "Programming Robots for Outer Space")
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
+        course = Course("Programming Robots for Outer Space", "Full-Time", "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
         assert(course.schedule == "Full-Time")
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course("Programming Robots for Outer Space", "Full-Time", "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
         assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
